d4b40fb5219d2d7f9e090b4a84597e7d162ea8db
khanos <epilef.rodriguez@gmail.com>
2017-07-26 19:21:06 -0400

    more configs for the dev env

---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+"use strict";
+
+var gulp = require('gulp');
+var connect = require('gulp-connect'); // Runs a local dev server
+var open = require('gulp-open'); // Open a URL in a web browser
+var browserify = require('browserify'); // Bundles JS
+var reactify = require('reactify'); // Transforms React JSX to JS
+var source = require('vinyl-source-stream'); // Use conventional text streams with gulp
+
+var config = {
+    port: 9005,
+    devBaseUrl: 'http://localhost',
+    path: {
+        html: './src/*.html',
+        js: './src/**/*.js',
+        dist: './dist',
+        mainJs: './src/main.js'
+    }
+};
+
+gulp.task('connect', function() {
+    connect.server({
+        root: ['dist'],
+        port: config.port,
+        base: config.devBaseUrl,
+        livereload: true
+    });
+});
+
+gulp.task('open', ['connect'], function() {
+    gulp.src('dist/index.html')
+        .pipe(open({ uri: config.devBaseUrl + ':' + config.port + '/'}));
+});
+
+gulp.task('html', function() {
+    gulp.src(config.path.html)
+        .pipe(gulp.dest(config.path.dist))
+        .pipe(connect.reload());
+});
+
+gulp.task('js', function() {
+    browserify(config.path.mainJs)
+        .transform(reactify)
+        .bundle()
+        .on('error', console.error.bind(console))
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest(config.path.dist  + '/scripts'))
+        .pipe(connect.reload());
+})
+
+gulp.task('watch', function() {
+    gulp.watch(config.path.html, ['html']);
+    gulp.watch(config.path.js, ['js']);
+});
+
+gulp.task('default', ['html', 'js', 'open', 'watch']);
---
+++ src/main.js
@@ -0,0 +1,3 @@
+var App = console.log('hello world from Browserify');
+
+module.exports = App;
